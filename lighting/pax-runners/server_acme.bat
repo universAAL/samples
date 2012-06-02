95d686f369959b0e7fa1fa98b73e90221ac6ab68
Vadim Eisenberg <vadime@il.ibm.com>
2012-06-02 17:18:54 +0000

    added pax-runners directory

---
+++ lighting/pax-runners/server_acme.bat
@@ -0,0 +1,1 @@
+pax-run --platform=felix --version=1.4.0 --startLevel=30 --dir=server_runner LightingExample_Distributed_ServerNotCoordinator_osgi_acme.pax_provision
---
+++ lighting/pax-runners/server_nomvn.bat
@@ -0,0 +1,1 @@
+pax-run --log=debug --platform=felix --version=1.4.0 --startLevel=30 --dir=server_runner LightingExample_Distributed_ServerNotCoordinator_osgi_nomaven.pax_provision
