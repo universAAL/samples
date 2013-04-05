6c8f542dc52a9deb1fc2c4337e86b94839689098
Ioannis Moulos <moulos@certh.gr>
2013-04-05 11:16:06 +0000

    ArtifactId & groupId fixed

--- smp.ri.exportComponent/ri.soap.server/src/main/java/org/universAAL/soap/cxf/server/ServiceImpl.java
+++ smp.ri.exportComponent/ri.soap.server/src/main/java/org/universAAL/soap/cxf/server/ServiceImpl.java
@@ -2,7 +2,6 @@ package org.universAAL.soap.cxf.server;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Hashtable;
 import java.util.List;
 
 import org.universAAL.middleware.container.utils.LogUtils;
