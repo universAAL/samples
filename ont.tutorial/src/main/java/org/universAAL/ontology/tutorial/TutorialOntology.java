74de302d19c7f40eee43f28f76afd9121524ffaf
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2013-06-11 00:11:57 +0000

    added license

--- ont.tutorial/src/main/java/org/universAAL/ontology/tutorial/TutorialOntology.java
+++ ont.tutorial/src/main/java/org/universAAL/ontology/tutorial/TutorialOntology.java
@@ -1,3 +1,24 @@
+/*
+	Copyright 2008-2014 ITACA-TSB, http://www.tsb.upv.es
+	Instituto Tecnologico de Aplicaciones de Comunicacion 
+	Avanzadas - Grupo Tecnologias para la Salud y el 
+	Bienestar (TSB)
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
 package org.universAAL.ontology.tutorial;
 
 import org.universAAL.middleware.owl.DataRepOntology;
@@ -13,7 +34,7 @@ import org.universAAL.ontology.phThing.PhThingOntology;
 import org.universAAL.ontology.phThing.Sensor;
 
 //This is the main central class where you actually define the restrictions and
-//relations between your concepts. It´s like a representation of an OWL file.
+//relations between your concepts. Itï¿½s like a representation of an OWL file.
 //You can have as many of these classes as you want in your bundle, 
 //but in general one is enough.
 public class TutorialOntology extends Ontology {
