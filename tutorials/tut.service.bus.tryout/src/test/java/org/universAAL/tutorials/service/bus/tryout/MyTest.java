99a911c6c5947ba51c829fedb813c23d6effc949
Carsten Stocklöw <stockloew@gmx.de>
2016-03-01 03:23:18 +0100

    added some calls to start the log monitor. These parts are commented because the LogMonitor is a tool and the dependency is not in samples.pm dep.mgmt (maybe change the ordering of repos in the release procedure to create tools before samples?). However, if the release procedure is completed, the dep can easily be added manually by uncommenting some parts.

--- tutorials/tut.service.bus.tryout/src/test/java/org/universAAL/tutorials/service/bus/tryout/MyTest.java
+++ tutorials/tut.service.bus.tryout/src/test/java/org/universAAL/tutorials/service/bus/tryout/MyTest.java
@@ -1,8 +1,25 @@
+/*
+    Copyright 2016-2020 Carsten Stockloew
+
+    See the NOTICE file distributed with this work for additional
+    information regarding copyright ownership
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+ */
 package org.universAAL.tutorials.service.bus.tryout;
 
-import org.universAAL.container.JUnit.JUnitModuleContext;
-import org.universAAL.container.JUnit.JUnitModuleContext.LogLevel;
-import org.universAAL.middleware.bus.junit.BusTestCase;
+import org.universAAL.container.JUnit.JUnitContainer;
+import org.universAAL.middleware.junit.MWTestCase;
 import org.universAAL.middleware.owl.OntologyManagement;
 import org.universAAL.middleware.service.DefaultServiceCaller;
 import org.universAAL.middleware.service.ServiceCaller;
@@ -17,7 +34,7 @@ import org.universAAL.ontology.phThing.PhThingOntology;
 import org.universAAL.ontology.shape.ShapeOntology;
 import org.universAAL.ontology.unit.UnitOntology;
 
-public class MyTest extends BusTestCase {
+public class MyTest extends MWTestCase {
 
     MyServiceCallee callee;
     ServiceCaller caller;
@@ -26,7 +43,6 @@ public class MyTest extends BusTestCase {
     protected void setUp() throws Exception {
 	// Initialization
 	super.setUp();
-	((JUnitModuleContext) mc).setLogLevel(LogLevel.ERROR);
 
 	// as we do not start the platform in a regular way, we have to register
 	// the required ontologies manually
@@ -40,6 +56,26 @@ public class MyTest extends BusTestCase {
 
 	callee = new MyServiceCallee(mc);
 	caller = new DefaultServiceCaller(mc);
+
+	// If you want to start the LogMonitor as well, add the following
+	// dependency to the pom file (adapt version number to latest):
+	// <dependency>
+	// <groupId>org.universAAL.tools</groupId>
+	// <artifactId>tools.log_mon</artifactId>
+	// <version>3.4.1-SNAPSHOT</version>
+	// </dependency>
+	// and start it by uncommenting the following lines:
+	
+	// org.universAAL.tools.logmonitor.Activator lm = new
+	// org.universAAL.tools.logmonitor.Activator();
+	// org.universAAL.tools.logmonitor.Activator.mc = mc;
+	// lm.start();
+	// ((JUnitContainer) mc.getContainer())
+	// .registerLogListeners(org.universAAL.tools.logmonitor.Activator.lm);
+	// org.universAAL.tools.logmonitor.service_bus_matching.LogMonitor.checkModule
+	// = false;
+
+	System.out.println(" - JUnit Framework started -");
     }
 
     /**
@@ -53,5 +89,14 @@ public class MyTest extends BusTestCase {
 	turnOn.addChangeEffect(new String[] { DeviceService.PROP_CONTROLS,
 		ValueDevice.PROP_HAS_VALUE }, new Integer(100));
 	caller.call(turnOn);
+
+	// // an endless loop to investigate output in LogMonitor
+	// while (true) {
+	// try {
+	// Thread.sleep(1000);
+	// } catch (InterruptedException e) {
+	// e.printStackTrace();
+	// }
+	// }
     }
 }
